5b5608fbaff3478df4c70c9b6610db4c1808cb20
devshum <57689318+devshum@users.noreply.github.com>
2022-05-10 11:42:21 +0300

    refactor app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { InlineSVGModule } from 'ng-inline-svg-2';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainModule } from './module-main/main.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MainModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    InlineSVGModule.forRoot({ baseUrl: '/assets/icons/', bypassHttpClientInterceptorChain: true })
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
